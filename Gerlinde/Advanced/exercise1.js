4583911687e18c54f961e17d63bbfe9b7d67eecd
Gerlinde <gerlinde@sandmair.com>
2022-05-25 13:59:56 +0200

    advanced finish

---
+++ Gerlinde/Advanced/exercise1.js
@@ -0,0 +1,30 @@
+var a = 7;
+
+var b = 14;
+
+var c = "21";
+
+var d = '36';
+
+var e = 42;
+
+let summ = a + b + c + d + e;
+
+console.log (summ);
+
+var f = '1';
+
+var g = 15;
+
+var h = 8;
+
+var i = "1";
+
+
+let multiply = f * g * h * i;
+
+console.log (multiply);
+
+
+let divide = summ / multiply;
+document.write(divide);
